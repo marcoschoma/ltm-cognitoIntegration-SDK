cc04fddccfde3d6e1de0d2a917e1875787dea7e2
Andre Ferreira Lima <andre.lima@grupoltm.com.br>
2019-12-18 18:37:48 -0300

    build

--- api/src/auth/auth.js
+++ api/src/auth/auth.js
@@ -1,11 +1,24 @@
-//const errorFactory = require('error-factory')
+const errorFactory = require('error-factory')
+const AuthError = errorFactory('Autherror')
 
-//const AuthError = errorFactory('Autherror')
+const { ok, badRequestWithMessage } = require('../config/response')
 
-const Discovery = async (redis, axios) => {
-  const discovery = await axios.get(process.env.COGNITO_DISCOVERY_ENDPOINT)
+const Discovery = async (response, redis, axios) => {
+  try {
+    const cacheKey = 'DiscoveryEndpoint'
 
-  redis.set('DiscoveryEndpoint', discovery.data, 'EX', 3600)
+    const cachedToken = await redis.get(cacheKey)
+
+    if (cachedToken) return ok(response, JSON.parse(cachedToken))
+
+    const discovery = await axios.get(process.env.COGNITO_DISCOVERY_ENDPOINT)
+
+    redis.set(cacheKey, JSON.stringify(discovery.data), 'EX', 7200)
+  } catch (error) {
+    throw AuthError(error)
+  }
 }
+
+const GetAuth = async (response, redis, axios) => {}
 
 module.exports = { Discovery }
